88ac7135dabff5dc02972ffba76faf1d020a02a6
Tuan Nguyen <tuannguyen08426@gmail.com>
2024-08-17 13:45:16 -0500

    C: main.py, changed main app into a class. Added basic grid configuration for textbox.

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from customtkinter import *
+import customtkinter as ctk                          # Import customtkinter as ctk abbreviation
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("325x225")
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
-app = CTk()
-app.geometry("500x400")
+        self.textbox = ctk.CTkTextbox(self, width=325, corner_radius=0)
+        self.textbox.grid(row=0, column=0, sticky="nsew")
 
+
+app = App()
 app.mainloop()
