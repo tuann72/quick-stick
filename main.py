306ab8de835244d9cb57adf3db41cc3798c39ba2
Tuan Nguyen <tuannguyen08426@gmail.com>
2024-08-17 23:22:40 -0500

    A: main.py, added button frame and button classes.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import customtkinter as ctk  # Import customtkinter as ctk abbreviation
 
+from customtkinter import CTkImage
+
+from PIL import Image
+
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
+
         # Sets the title of the window
         self.title("Quick Stick")
 
@@ -16,16 +21,44 @@ class App(ctk.CTk):
 
         self.grid_columnconfigure(0, weight=1)  # Single column to contain all items
 
-        # Creating a frame to hold all button options
-        self.btnFrame = ctk.CTkFrame(self, fg_color="red", height=40, corner_radius=0)
-        self.btnFrame.grid(row=0, column=0, sticky="nsew")
-
-        self.btn = ctk.CTkButton(self.btnFrame, text="Button", width=20, height=20)
-        self.btn.grid(row=0, column=0, padx=10)
+        self.btnFrame = TextOptionFrame(self)
+        self.btnFrame.grid(
+            row=0,
+            column=0,
+        )
 
         # Creating a textbox to hold user input
         self.textbox = ctk.CTkTextbox(self, width=325, corner_radius=0)
         self.textbox.grid(row=1, column=0, sticky="nsew")
+
+
+class TextOptionFrame(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        boldImg = CTkImage(Image.open("assets/bold.png"))
+        fontImg = CTkImage(Image.open("assets/font.png"))
+        italicImg = CTkImage(Image.open("assets/italic.png"))
+
+        # Creating a frame to hold all button options
+        self.btnFrame = ctk.CTkFrame(self, fg_color="red", height=10, corner_radius=0)
+        self.btnFrame.grid(row=0, column=0)
+
+        self.fontBtn = BtnOptionModule(self.btnFrame, fontImg)
+        self.fontBtn.pack(side="left", padx="5")
+
+        self.boldBtn = BtnOptionModule(self.btnFrame, boldImg)
+        self.boldBtn.pack(side="left", padx="5")
+
+        self.italicBtn = BtnOptionModule(self.btnFrame, italicImg)
+        self.italicBtn.pack(side="left", padx="5")
+
+
+class BtnOptionModule(ctk.CTkButton):
+    def __init__(self, master, icon, command=None):
+        super().__init__(
+            master, image=icon, text="", command=command, width=20, height=20
+        )
 
 
 app = App()
