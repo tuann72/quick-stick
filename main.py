0102c6195897f1b80cd788b50d74264985f129f9
Tuan Nguyen <tuannguyen08426@gmail.com>
2024-08-18 17:57:41 -0500

    C: main.py, made significant changes in regards to buttons, removed the text button options and will now move forward with a right click to edit style. Created a simple title bar with basic icons. Created 3 functions to minimize, maximize, and close window.

--- main.py
+++ main.py
@@ -2,16 +2,13 @@ import customtkinter as ctk  # Import customtkinter as ctk abbreviation
 from customtkinter import CTkImage  # Import CTkImage
 from PIL import Image
 
-ctk.set_appearance_mode("dark")
+TITLEBAR_FG_COLOR = "#fff170"
+TEXTBOX_FG_COLOR = "#fffea1"
 
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
-
-        boldImg = CTkImage(Image.open("assets/bold.png"))
-        fontImg = CTkImage(Image.open("assets/font.png"))
-        italicImg = CTkImage(Image.open("assets/italic.png"))
 
         # Sets the title of the window
         self.title("Quick Stick")
@@ -19,42 +16,71 @@ class App(ctk.CTk):
         # Creates starting layout size
         self.geometry("325x225")
 
-        # Configures a grids' rows and columns
-        self.grid_rowconfigure(0, weight=0)  # Set for button options to be fixed size
-        self.grid_rowconfigure(1, weight=1)  # Set for textbox to be resizable
+        self.rowconfigure(0, weight=0)
+        self.rowconfigure(1, weight=1)
+        self.columnconfigure(0, weight=1)
 
-        self.grid_columnconfigure(0, weight=1)  # Single column to contain all items
+        # self.overrideredirect(True)
 
-        # self.btnFrame = TextOptionFrame(self)
-        self.btnFrame = ctk.CTkFrame(self, height=10, corner_radius=0, fg_color="red")
-        self.btnFrame.grid(row=0, column=0, sticky="ew")
+        minImg = CTkImage(Image.open("assets/min.png"), size=(10, 10))
+        maxImg = CTkImage(Image.open("assets/max.png"), size=(10, 10))
+        closeImg = CTkImage(Image.open("assets/close.png"), size=(10, 10))
 
-        # Center the frame in the window
-        self.btnFrame.grid_columnconfigure(0, weight=1)
-        self.btnFrame.grid_columnconfigure(1, weight=1)
-        self.btnFrame.grid_columnconfigure(2, weight=1)
+        self.titleBar = ctk.CTkFrame(
+            self, fg_color=TITLEBAR_FG_COLOR, height=15, corner_radius=0
+        )
+        self.titleBar.grid(row=0, column=0, sticky="ew")
 
-        self.fontBtn = BtnOptionModule(self.btnFrame, fontImg)
-        self.boldBtn = BtnOptionModule(self.btnFrame, boldImg)
-        self.italicBtn = BtnOptionModule(self.btnFrame, italicImg)
+        self.titleBarLabel = ctk.CTkLabel(
+            self.titleBar,
+            height=14,
+            text="gjp",
+            font=("Times", 14),
+        )
+        self.titleBarLabel.pack(side="left", padx=12, pady=2)
 
-        ##### Fix Padding Issue
+        self.minBtn = BtnOptionModule(self.titleBar, minImg, self.min_window)
+        self.maxBtn = BtnOptionModule(self.titleBar, maxImg, self.max_window)
+        self.closeBtn = BtnOptionModule(self.titleBar, closeImg, self.close_window)
 
-        self.fontBtn.grid(row=0, column=0, padx=1)
-        self.boldBtn.grid(row=0, column=1, padx=1)
-        self.italicBtn.grid(row=0, column=2, padx=1)
+        self.closeBtn.pack(side="right", padx=2)
+        self.maxBtn.pack(side="right", padx=2)
+        self.minBtn.pack(side="right", padx=2)
 
         # Creating a textbox to hold user input
-        self.textbox = ctk.CTkTextbox(self, width=325, corner_radius=0)
+        self.textbox = ctk.CTkTextbox(
+            self, fg_color=TEXTBOX_FG_COLOR, width=325, corner_radius=0
+        )
         self.textbox.grid(row=1, column=0, sticky="nsew")
+
+    def close_window(self):
+        self.destroy()
+
+    def max_window(self):
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        self.geometry(f"{screen_width}x{screen_height}+{0}+{0}")
+
+    def min_window(self):
+        self.iconify()
 
 
 class BtnOptionModule(ctk.CTkButton):
     def __init__(self, master, icon, command=None):
         super().__init__(
-            master, image=icon, text="", command=command, width=20, height=20
+            master,
+            image=icon,
+            border_spacing=0,
+            text="",
+            command=command,
+            width=20,
+            height=20,
+            corner_radius=0,
+            fg_color="transparent",
+            hover_color="#e3e3e3",
         )
 
 
-app = App()
-app.mainloop()
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
