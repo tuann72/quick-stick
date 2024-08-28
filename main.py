f48068aca503109df53bbe46d63f826dabca8852
Tuan Nguyen <tuannguyen08426@gmail.com>
2024-08-27 23:44:51 -0500

    A: main.py, added theme changing functions. Made maximize cleaner, unmaximizing returns it to it's original state.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import customtkinter as ctk  # Import customtkinter as ctk abbreviation
 import tkinter as tk
 from customtkinter import CTkImage  # Import CTkImage
-from PIL import Image, ImageDraw
+from PIL import Image
 from pystray import MenuItem as item, Menu as menu
 import pystray
-import time
 
 from settings import (
     TITLE_BAR_COLOR_LIST,
@@ -13,15 +12,14 @@ from settings import (
     TITLE_BAR_SIZE,
 )
 
-COLOR_CYCLE = 0
-TITLE_BAR_COLOR = TITLE_BAR_COLOR_LIST[COLOR_CYCLE]
-TEXTBOX_COLOR = TEXTBOX_COLOR_LIST[COLOR_CYCLE]
-BUTTON_HOVER_COLOR = BUTTON_HOVER_COLOR_LIST[COLOR_CYCLE]
-
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
+        self.COLOR_CYCLE = 0
+        self.TITLE_BAR_COLOR = TITLE_BAR_COLOR_LIST[self.COLOR_CYCLE]
+        self.TEXTBOX_COLOR = TEXTBOX_COLOR_LIST[self.COLOR_CYCLE]
+        self.BUTTON_HOVER_COLOR = BUTTON_HOVER_COLOR_LIST[self.COLOR_CYCLE]
 
         minImg = CTkImage(Image.open("assets/min.png"), size=(12, 12))
         maxImg = CTkImage(Image.open("assets/max.png"), size=(12, 12))
@@ -32,6 +30,8 @@ class App(ctk.CTk):
         self.titleBarLabelStr = ""
         self.curr_width = 300
         self.curr_height = 200
+        self.prev_x = 0
+        self.prev_y = 0
 
         # Sets the title of the window
         self.title("Quick Stick")
@@ -40,7 +40,7 @@ class App(ctk.CTk):
         self.geometry("300x200")
 
         # Changes background color
-        self.configure(fg_color=TEXTBOX_COLOR)
+        self.configure(fg_color=self.TEXTBOX_COLOR)
 
         self.rowconfigure(0, weight=0)
         self.rowconfigure(1, weight=1)
@@ -50,7 +50,7 @@ class App(ctk.CTk):
         self.overrideredirect(True)
 
         self.titleBar = ctk.CTkFrame(
-            self, fg_color=TITLE_BAR_COLOR, height=TITLE_BAR_SIZE, corner_radius=0
+            self, fg_color=self.TITLE_BAR_COLOR, height=TITLE_BAR_SIZE, corner_radius=0
         )
         self.titleBar.grid(row=0, column=0, sticky="ew")
 
@@ -76,9 +76,15 @@ class App(ctk.CTk):
         self.titleBarLabel.bind("<B1-Motion>", self.move_window)
 
         # Create buttons to minimize, maximize, and close the window
-        self.minBtn = BtnOptionModule(self.titleBar, minImg, self.min_window)
-        self.maxBtn = BtnOptionModule(self.titleBar, maxImg, self.max_window)
-        self.closeBtn = BtnOptionModule(self.titleBar, closeImg, self.close_window)
+        self.minBtn = BtnOptionModule(
+            self.titleBar, minImg, self.min_window, self.BUTTON_HOVER_COLOR
+        )
+        self.maxBtn = BtnOptionModule(
+            self.titleBar, maxImg, self.max_window, self.BUTTON_HOVER_COLOR
+        )
+        self.closeBtn = BtnOptionModule(
+            self.titleBar, closeImg, self.close_window, self.BUTTON_HOVER_COLOR
+        )
 
         # Packs the buttons to the right side of the title bar
         self.closeBtn.pack(side="right")
@@ -89,7 +95,7 @@ class App(ctk.CTk):
         self.textbox = ctk.CTkTextbox(
             self,
             font=("Helvetica", 16),
-            fg_color=TEXTBOX_COLOR,
+            fg_color=self.TEXTBOX_COLOR,
             text_color="black",
             width=325,
             corner_radius=0,
@@ -104,8 +110,8 @@ class App(ctk.CTk):
             width=8,
             height=8,
             corner_radius=0,
-            fg_color=TEXTBOX_COLOR,
-            hover_color=BUTTON_HOVER_COLOR,
+            fg_color=self.TEXTBOX_COLOR,
+            hover_color=self.BUTTON_HOVER_COLOR,
         )
         self.resizeBtn.grid(row=2, column=0, sticky="e")
         self.resizeBtn.bind("<Button-1>", self.on_drag_start)
@@ -134,6 +140,7 @@ class App(ctk.CTk):
 
     # Resizes window when the bottom right button is selected and dragged
     def resize_window(self, event):
+        # Add the difference of mouse movement to current size.
         new_x_size = self.winfo_width() + event.x - self.drag_start_x
         new_y_size = self.winfo_height() + event.y - self.drag_start_y
         if new_x_size >= 300 and new_y_size >= 60:
@@ -153,9 +160,17 @@ class App(ctk.CTk):
     def max_window(self):
         # If already maximized we return it to original size
         if self.maximized:
-            self.geometry("300x200")
+            self.geometry(
+                f"{self.curr_width}x{self.curr_height}+{self.prev_x}+{self.prev_y}"
+            )
             self.maximized = False
         else:
+            # Record current size
+            self.curr_width = self.winfo_width()
+            self.curr_height = self.winfo_height()
+            self.prev_x = self.winfo_rootx()
+            self.prev_y = self.winfo_rooty()
+
             # Sets screen to max size and moves window to the top left
             self.geometry(
                 f"{self.winfo_screenwidth()}x{self.winfo_screenheight()}+{0}+{0}"
@@ -165,28 +180,6 @@ class App(ctk.CTk):
     # Command for min button to minimize window
     def min_window(self):
         self.withdraw()
-
-    # Restores the window back to the screen
-    def restore(self):
-        self.deiconify()  # Show the window
-        self.update_idletasks()  # Ensure the window is restored with the correct geometry
-
-    # Create menu options for system tray icon
-    def create_menu(self):
-        # Creates two options, a restore option and a quit option.
-        return (
-            item("Restore", self.restore),
-            item(
-                "Themes",
-                menu(
-                    item("Pink", lambda test: 2),
-                    item("Purple", lambda test: 2),
-                    item("Yellow", lambda test: 2),
-                    item("Blue", lambda test: 2),
-                    item("Green", lambda test: 2),
-                ),
-            ),
-        )
 
     # Command for collapse button to collapse window
     def collapse_window(self):
@@ -216,6 +209,65 @@ class App(ctk.CTk):
             # Set the state of minimized to be true
             self.minimized = True
 
+    # Restores the window back to the screen
+    def restore(self):
+        self.deiconify()  # Show the window
+        self.update_idletasks()  # Ensure the window is restored with the correct geometry
+
+    # Create menu options for system tray icon
+    def create_menu(self):
+        return (
+            item("Restore", self.restore),
+            item(
+                "Themes",
+                menu(
+                    item("Pink", lambda: self.changeTheme("Pink")),
+                    item("Purple", lambda: self.changeTheme("Purple")),
+                    item("Yellow", lambda: self.changeTheme("Yellow")),
+                    item("Blue", lambda: self.changeTheme("Blue")),
+                    item("Green", lambda: self.changeTheme("Green")),
+                ),
+            ),
+        )
+
+    # Change window theme color
+    def changeTheme(self, color):
+        if color == "Pink":
+            self.COLOR_CYCLE = 0
+        elif color == "Purple":
+            self.COLOR_CYCLE = 1
+        elif color == "Yellow":
+            self.COLOR_CYCLE = 2
+        elif color == "Blue":
+            self.COLOR_CYCLE = 3
+        elif color == "Green":
+            self.COLOR_CYCLE = 4
+
+        self.TITLE_BAR_COLOR = TITLE_BAR_COLOR_LIST[self.COLOR_CYCLE]
+        self.TEXTBOX_COLOR = TEXTBOX_COLOR_LIST[self.COLOR_CYCLE]
+        self.BUTTON_HOVER_COLOR = BUTTON_HOVER_COLOR_LIST[self.COLOR_CYCLE]
+
+        self.updateColors()
+
+    def updateColors(self):
+        # Update main window
+        self.configure(fg_color=self.TEXTBOX_COLOR)
+
+        # Update title bar
+        self.titleBar.configure(fg_color=self.TITLE_BAR_COLOR)
+
+        # Update textbox
+        self.textbox.configure(fg_color=self.TEXTBOX_COLOR)
+
+        # Update resize button
+        self.resizeBtn.configure(
+            fg_color=self.TEXTBOX_COLOR, hover_color=self.BUTTON_HOVER_COLOR
+        )
+
+        # Update other buttons
+        for btn in [self.minBtn, self.maxBtn, self.closeBtn]:
+            btn.configure(hover_color=self.BUTTON_HOVER_COLOR)
+
     # Updates the title bar's label which correlates to the first line of the textbox
     def update_title(self, event):
         # we get the last index of the 1st line
@@ -229,7 +281,7 @@ class App(ctk.CTk):
 
 
 class BtnOptionModule(ctk.CTkButton):
-    def __init__(self, master, icon, command=None):
+    def __init__(self, master, icon, command=None, hover_color=None):
         super().__init__(
             master,
             image=icon,
@@ -240,7 +292,7 @@ class BtnOptionModule(ctk.CTkButton):
             height=20,
             corner_radius=0,
             fg_color="transparent",
-            hover_color=BUTTON_HOVER_COLOR,
+            hover_color=hover_color,
         )
 
 
